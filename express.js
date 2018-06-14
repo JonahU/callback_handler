3cd26a5dc97ced60802d6cc8da797e7464c9d711
Jonah U <ju374@nyu.edu>
2018-06-14 19:29:07 -0400

    Refactored redirect code to allow for alternative redirects

--- express.js
+++ express.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const fs = require('fs');
 const https = require('https');
-const path = require('path');
 const url = require('url');
 const Cognito = require('./cognito');
 const config = require('./config');
@@ -20,26 +18,26 @@ app.get('/', (req, res) => {
             url: () => { return config.hostedAuthPage(); }
         }
     });
-})
+});
 
 app.get('/callback', (req, res) => {
     Cognito.getAuthCode(req.query)
-    .then(code => Cognito.fetchToken(code))
-    .then(token => Cognito.verifyToken(token))
-    .then(token => Cognito.getGroup(token))
-    .then(([group, accessToken, overrideRedirect]) => {
-        switch(group) {
-            case config.groupA():
-                handleRedirect(res, accessToken, overrideRedirect);
-                break;
-            case config.groupB():
-                handleRedirect(res, accessToken, overrideRedirect);
-                break; 
-            default:
-                throw new Error('Unknown group');
-        }        
-    })
-    .catch(err => res.send(err.message))
+        .then(code => Cognito.fetchToken(code))
+        .then(token => Cognito.verifyToken(token))
+        .then(token => Cognito.getGroup(token))
+        .then(([group, accessToken, overrideRedirect]) => {
+            switch(group) {
+                case config.groupA():
+                    handleRedirect(res, accessToken, group, overrideRedirect);
+                    break;
+                case config.groupB():
+                    handleRedirect(res, accessToken, group, overrideRedirect);
+                    break; 
+                default:
+                    throw new Error('Unknown group');
+            }        
+        })
+        .catch(err => res.send(err.message))
 });
 
 app.get(`/${config.pathA()}`, (req, res) => {
@@ -62,18 +60,31 @@ app.get(`/${config.pathB()}`, (req, res) => {
     });
 });
 
-const handleRedirect = (res, accessToken, overrideRedirect) => {
-    if (overrideRedirect) {
-        res.set({
-            jwt: accessToken.token.id_token,
-            refresh: accessToken.token.refresh_token
-        });
-        res.redirect(302, overrideRedirect);
-    } else {
-        res.redirect(302, url.format({
-            pathname: `/${config.pathA()}`,
-            query: accessToken.decodedToken
-        }));
+const externalRedirect = (res, accessToken, url) => {
+    res.set({
+        jwt: accessToken.token.id_token,
+        refresh: accessToken.token.refresh_token
+    });
+    res.redirect(302, url);
+}
+
+const internalRedirect = (res, accessToken, group) => {
+    res.redirect(302, url.format({
+        pathname: `/${config.pathForGroup(group)}`,
+        query: accessToken.decodedToken
+    }));
+}
+
+const handleRedirect = (res, accessToken, group, overrideRedirect) => {
+    switch (overrideRedirect) {
+        case config.redirectA():
+            externalRedirect(res, accessToken, overrideRedirect)
+            break;
+        case config.redirectB():
+            externalRedirect(res, accessToken, overrideRedirect)
+            break;
+        default:
+            internalRedirect(res, accessToken, group)
     }
 }
 
